5e934faad5e45df39a8c73aa0f59fd402ecf5ec1
PrzemekMiros <kontakt@futurewebstudio.pl>
2023-06-11 07:34:02 +0100

    Add netlify CMS

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/js");
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("src/admin");
 
     return {
       dir: {
