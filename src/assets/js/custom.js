2379578423501609d6bf522f1c58a0abb654600f
PrzemekMiros <kontakt@futurewebstudio.pl>
2023-06-16 19:22:18 +0100

    Add mobile menu

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -177,3 +177,58 @@ if (window.matchMedia("(min-width: 767px)").matches) {
         }); 
     });
   }
+
+
+
+  
+// Navigation
+
+var menuToggle = document.getElementById("menuToggle");
+
+var menuBar = gsap.timeline({ paused: true});
+
+menuBar.to('.bar-1', 1,{
+	attr:{d: "M8,2 L2,8"},
+	x: 1,
+	ease: Power2.easeInOut
+}, 'start')
+
+menuBar.to('.bar-2', 1,{
+	autoAlpha: 0
+}, 'start')
+
+menuBar.to('.bar-3', 1,{
+	attr:{d: "M8,8 L2,2"},
+	x: 1,
+	ease: Power2.easeInOut
+}, 'start')
+
+menuBar.reverse();
+
+var navTl = gsap.timeline({ paused:true });
+
+navTl.to('.fullpage-menu', {
+	duration: 0,
+	display: "block",
+	ease: Expo.easeInOut
+}, "<");
+
+navTl.to('.menu-bg', {
+	duration: 1,
+	opacity: 1,
+	ease: Expo.easeInOut
+}, "<");
+
+navTl.to('.menu li a', {
+	duration: 1.5,
+	y: 0,
+	stagger: 0.05,
+	ease: Power3.easeInOut 
+}, "-=0.8");
+
+navTl.reverse();
+
+menuToggle.addEventListener('click', function(){
+	menuBar.reversed(!menuBar.reversed());
+	navTl.reversed(!navTl.reversed());
+});
