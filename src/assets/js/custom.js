690add67ce37cbee237b07dcdc3074fa75db6588
PrzemekMiros <kontakt@futurewebstudio.pl>
2023-06-20 19:56:26 +0200

    Fix

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -290,32 +290,3 @@ circleContent0.addEventListener("mouseenter", () => {
         circleContent2.classList.remove("active");
         });
       
-
-
-
-        const changeTheme = document.querySelector(".change-theme");
-        let theme = localStorage.getItem("theme");
-        
-        
-        changeTheme.addEventListener('click', () => {
-            if (theme === "dark") {
-                document.querySelector("body").classList.remove("dark");
-                document.querySelector("body").classList.add("light");
-                theme = "light";
-            } else {
-                document.querySelector("body").classList.add("dark");
-                document.querySelector("body").classList.remove("light");
-                theme = "dark";
-            }
-        
-            localStorage.setItem("theme", theme);
-        });
-        
-        
-        if (theme === "dark") {
-            document.querySelector("body").classList.add("dark");
-        }
-        
-        if (theme === "light") {
-            document.querySelector("body").classList.add("light");
-        }
